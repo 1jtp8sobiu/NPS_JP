2c96d589e1ef2240a8bc640c94891cbdc5b79718
1jtp8sobiu <3oxfoq0y@gmail.com>
2020-03-24 21:57:38 +0900

    Update tsv files

--- rearrange-upload.bat
+++ rearrange-upload.bat
@@ -1,41 +1,41 @@
 
-set URL="https://docs.google.com/spreadsheets/d/1-qPn38KJNYmlnN4ZgYnz7DFWZZmyN_ncKW-Cvlts8TM/export?format=tsv&gid=402478617"
+set URL="https://docs.google.com/spreadsheets/d/1auvU8eCCyTcSuuBe-upj4_eCrkQWcGrAaTi1ZLy6CR0/export?format=tsv&gid=0"
 curl %URL% > rearrange\PSV_APP.tsv
 
-set URL="https://docs.google.com/spreadsheets/d/1-qPn38KJNYmlnN4ZgYnz7DFWZZmyN_ncKW-Cvlts8TM/export?format=tsv&gid=1516154307"
+set URL="https://docs.google.com/spreadsheets/d/1auvU8eCCyTcSuuBe-upj4_eCrkQWcGrAaTi1ZLy6CR0/export?format=tsv&gid=1839974110"
+curl %URL% > rearrange\PSV_AC.tsv
+
+set URL="https://docs.google.com/spreadsheets/d/1auvU8eCCyTcSuuBe-upj4_eCrkQWcGrAaTi1ZLy6CR0/export?format=tsv&gid=1731336597"
+curl %URL% > rearrange\PSV_THEME.tsv
+
+
+set URL="https://docs.google.com/spreadsheets/d/1auvU8eCCyTcSuuBe-upj4_eCrkQWcGrAaTi1ZLy6CR0/export?format=tsv&gid=1978174717"
 curl %URL% > rearrange\PS3_APP.tsv
 
-set URL="https://docs.google.com/spreadsheets/d/1-qPn38KJNYmlnN4ZgYnz7DFWZZmyN_ncKW-Cvlts8TM/export?format=tsv&gid=634429192"
+set URL="https://docs.google.com/spreadsheets/d/1auvU8eCCyTcSuuBe-upj4_eCrkQWcGrAaTi1ZLy6CR0/export?format=tsv&gid=212528288"
+curl %URL% > rearrange\PS3_AC.tsv
+
+
+set URL="https://docs.google.com/spreadsheets/d/1auvU8eCCyTcSuuBe-upj4_eCrkQWcGrAaTi1ZLy6CR0/export?format=tsv&gid=1407082301"
 curl %URL% > rearrange\PSP_APP.tsv
 
-set URL="https://docs.google.com/spreadsheets/d/1-qPn38KJNYmlnN4ZgYnz7DFWZZmyN_ncKW-Cvlts8TM/export?format=tsv&gid=1363573480"
+set URL="https://docs.google.com/spreadsheets/d/1auvU8eCCyTcSuuBe-upj4_eCrkQWcGrAaTi1ZLy6CR0/export?format=tsv&gid=748612071"
+curl %URL% > rearrange\PSP_AC.tsv
+
+
+set URL="https://docs.google.com/spreadsheets/d/1auvU8eCCyTcSuuBe-upj4_eCrkQWcGrAaTi1ZLy6CR0/export?format=tsv&gid=1047764865"
 curl %URL% > rearrange\PSX_APP.tsv
 
 
 
-set URL="https://docs.google.com/spreadsheets/d/1-qPn38KJNYmlnN4ZgYnz7DFWZZmyN_ncKW-Cvlts8TM/export?format=tsv&gid=908332530"
-curl %URL% > rearrange\PSV_AC.tsv
 
-set URL="https://docs.google.com/spreadsheets/d/1-qPn38KJNYmlnN4ZgYnz7DFWZZmyN_ncKW-Cvlts8TM/export?format=tsv&gid=1285591494"
-curl %URL% > rearrange\PS3_AC.tsv
-
-set URL="https://docs.google.com/spreadsheets/d/1-qPn38KJNYmlnN4ZgYnz7DFWZZmyN_ncKW-Cvlts8TM/export?format=tsv&gid=1797597087"
-curl %URL% > rearrange\PSP_AC.tsv
-
-
-
-set URL="https://docs.google.com/spreadsheets/d/1-qPn38KJNYmlnN4ZgYnz7DFWZZmyN_ncKW-Cvlts8TM/export?format=tsv&gid=1235712945"
-curl %URL% > rearrange\PSV_THEME.tsv
-
-
-
-set URL="https://docs.google.com/spreadsheets/d/1-qPn38KJNYmlnN4ZgYnz7DFWZZmyN_ncKW-Cvlts8TM/export?format=tsv&gid=354266861"
+set URL="https://docs.google.com/spreadsheets/d/1auvU8eCCyTcSuuBe-upj4_eCrkQWcGrAaTi1ZLy6CR0/export?format=tsv&gid=1696721134"
 curl %URL% > rearrange\pkgi_dlcs.txt
 
-set URL="https://docs.google.com/spreadsheets/d/1-qPn38KJNYmlnN4ZgYnz7DFWZZmyN_ncKW-Cvlts8TM/export?format=tsv&gid=291590742"
+set URL="https://docs.google.com/spreadsheets/d/1auvU8eCCyTcSuuBe-upj4_eCrkQWcGrAaTi1ZLy6CR0/export?format=tsv&gid=708427661"
 curl %URL% > rearrange\pkgi_avatars.txt
 
-set URL="https://docs.google.com/spreadsheets/d/1-qPn38KJNYmlnN4ZgYnz7DFWZZmyN_ncKW-Cvlts8TM/export?format=tsv&gid=1927354210"
+set URL="https://docs.google.com/spreadsheets/d/1auvU8eCCyTcSuuBe-upj4_eCrkQWcGrAaTi1ZLy6CR0/export?format=tsv&gid=163582373"
 curl %URL% > rearrange\pkgi_themes.txt
 
 
