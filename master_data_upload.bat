f5d1d68c927caaa9fc619495e559ea2ccf500301
1jtp8sobiu <3oxfoq0y@gmail.com>
2020-03-26 03:23:48 +0900

    Update tsv files

--- master_data_upload.bat
+++ master_data_upload.bat
@@ -1,15 +1,9 @@
-set URL="https://docs.google.com/spreadsheets/d/1auvU8eCCyTcSuuBe-upj4_eCrkQWcGrAaTi1ZLy6CR0/export?format=tsv&gid=473246730"
-curl %URL% > rearrange\master_data\pkg_meta.tsv
+move "Y:\downlods\psn_pkg_database - pkg_meta.tsv" rearrange\master_data\pkg_meta.tsv
+move "Y:\downlods\psn_pkg_database - zrif.tsv" rearrange\master_data\zrif.tsv
+move "Y:\downlods\psn_pkg_database - zrif_psm.tsv" rearrange\master_data\zrif_psm.tsv
+move "Y:\downlods\psn_pkg_database - rap.tsv" rearrange\master_data\rap.tsv
 
-set URL="https://docs.google.com/spreadsheets/d/1auvU8eCCyTcSuuBe-upj4_eCrkQWcGrAaTi1ZLy6CR0/export?format=tsv&gid=259406114"
-curl %URL% > rearrange\master_data\zrif.tsv
-
-set URL="https://docs.google.com/spreadsheets/d/1auvU8eCCyTcSuuBe-upj4_eCrkQWcGrAaTi1ZLy6CR0/export?format=tsv&gid=140794933"
-curl %URL% > rearrange\master_data\zrif_psm.tsv
-
-set URL="https://docs.google.com/spreadsheets/d/1auvU8eCCyTcSuuBe-upj4_eCrkQWcGrAaTi1ZLy6CR0/export?format=tsv&gid=118405442"
-curl %URL% > rearrange\master_data\rap.tsv
-
+pause
 git add .
 git commit -a -m "Update tsv files"
 git push origin master 
